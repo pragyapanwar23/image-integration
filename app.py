ad51ca88465f9ca47eb76e2792a03c4f45499aa1
Pragya Panwar <98818480+pragyapanwar23@users.noreply.github.com>
2025-06-15 01:06:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+%%writefile app.py
+import streamlit as st
+from PIL import Image
+
+st.title("Final Composited Image Viewer")
+st.markdown("**Note:** This deployment can only display the final image formed at the end of the model due to time constraints.")
+# Load and show image
+image_path = "final_result_grounded.jpg"  # Update with the path to your generated image
+img = Image.open(image_path)
+st.image(img, caption="Seamlessly Integrated Image", width=350)
+
