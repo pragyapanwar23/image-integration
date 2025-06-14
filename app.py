a9cdb129b24b00db12e570fb6c4810f2eb6e137c
Pragya Panwar <98818480+pragyapanwar23@users.noreply.github.com>
2025-06-15 01:21:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Final Composited Image Viewer")
 st.markdown("**Note:** This deployment can only display the final image formed at the end of the model due to time constraints.")
 # Load and show image
-image_path = "final_result_grounded.jpg"  # Update with the path to your generated image
+image_path = "final_result.png"  # Update with the path to your generated image
 img = Image.open(image_path)
 st.image(img, caption="Seamlessly Integrated Image", width=350)
 
